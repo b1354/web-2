4b8e2158c34f6be509e3d77a3ed5e728d2e5d681
b1354 <bayurizkyseptiansyah@gmail.com>
2024-10-01 14:39:58 +0700

    first commit

---
+++ latihan.php
@@ -0,0 +1,8 @@
+<?php
+    echo "Nama Saya: Bayu rizki <br>";
+    echo "Alamat: Baleendah <br>";
+    echo "<hr>";
+    echo "<center>";
+    echo "<img src='./robot.png' align='center' style='margin:auto;'>";
+    echo "</center>";
+?>
